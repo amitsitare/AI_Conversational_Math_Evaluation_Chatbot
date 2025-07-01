a526dcaeee879a001f122d19a6858f029ac1c5a3
Amit Diwakar <amitdiwakar946@gmail.com>
2025-07-01 11:10:29 +0530

    fixed the frontedn path in backend

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 # Allow your Vercel frontend to make requests to this backend
 cors_origins = [
-    "https://ai-conversational-math-evaluation-c-two.vercel.app",
+    "https://ai-conversational-math-evaluation-c.vercel.app",
     "http://localhost:3000" # Also allow local development
 ]
 
